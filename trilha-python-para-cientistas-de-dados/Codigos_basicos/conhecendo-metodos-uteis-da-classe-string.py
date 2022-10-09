6e8540948b95494122b76ace63062340f2da39ee
AlineGorischF <aline.gorisch@outlook.com>
2022-10-09 09:06:47 -0300

    Alocação materiais de apoio e inicio curso 5 em python

---
+++ trilha-python-para-cientistas-de-dados/Codigos_basicos/conhecendo-metodos-uteis-da-classe-string.py
@@ -0,0 +1,30 @@
+# **Maiúscula, minúscula e título**
+
+# nome = "NiMbuS"
+
+# print(nome.upper())
+# print(nome.lower())
+# print(nome.title())
+
+# **Eliminando espaços em branco**
+
+# texo = "  Olá, mundo    "
+# # print(texo)
+
+# print(texo.strip()+ ".")
+# print(texo.lstrip()+ ".")
+# print(texo.rstrip()+ ".")
+
+# **Junções e centralização**
+
+menu = "Python"
+
+# Centralização
+
+# print("####" + menu + "####")
+# print(menu.center(14))
+# print(menu.center(14, "#"))
+
+# Join
+
+print( " . " . join(menu))
