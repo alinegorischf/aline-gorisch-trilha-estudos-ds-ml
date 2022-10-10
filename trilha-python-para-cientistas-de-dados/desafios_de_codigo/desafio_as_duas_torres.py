a9197e530ae51997c9175f7fcca1459f2dad22b1
AlineGorischF <aline.gorisch@outlook.com>
2022-10-10 16:45:30 -0300

    Desafio de código 1

--- trilha-python-para-cientistas-de-dados/desafios_de_codigo/desafio_as_duas_torres.py
+++ trilha-python-para-cientistas-de-dados/desafios_de_codigo/desafio_as_duas_torres.py
@@ -6,10 +6,18 @@
 # em cada parâmetro da função ou com os valores predefinidos por padrão;
 
 # Abaixo segue um exemplo de código que você pode ou não utilizar
+
 entrada = input().split()
 
-distancia = int(entrada[0])
-diametro1 = int(entrada[1])
-diametro2 = int(entrada[2])
+
+distancia = int(entrada[100])
+diametro1 = int(entrada[20])
+diametro2 = int(entrada[30])
+
 
 # TODO: Calcule o ICM da comunicação dos Palatír de Sauron e Saruman, com 2 casas decimais no espaço #em branco abaixo:
+
+if ( (distancia > 0 and distancia < 10000) and (diametro1 > 0 and diametro1 <100) and (diametro2 > 0 and diametro2 < 100) ):
+    
+    ICM = distancia / (diametro1 + diametro2)
+    print(f"{ICM:.2f}")
